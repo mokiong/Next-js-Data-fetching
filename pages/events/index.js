fb9624f73713b6cf1ce653b9d2116063e0a26a64
Moki <mokiong1427@gmail.com>
2021-10-23 11:49:42 +0800

    Get props on build time

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,26 +1,37 @@
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
-function AllEventsPage() {
-  const router = useRouter();
-  const events = getAllEvents();
+function AllEventsPage(props) {
+    const router = useRouter();
+    const { events } = props;
 
-  function findEventsHandler(year, month) {
-    const fullPath = `/events/${year}/${month}`;
-    
-    router.push(fullPath);
-  }
+    function findEventsHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`;
 
-  return (
-    <Fragment>
-      <EventsSearch onSearch={findEventsHandler} />
-      <EventList items={events} />
-    </Fragment>
-  );
+        router.push(fullPath);
+    }
+
+    return (
+        <Fragment>
+            <EventsSearch onSearch={findEventsHandler} />
+            <EventList items={events} />
+        </Fragment>
+    );
+}
+
+export async function getStaticProps() {
+    const events = await getAllEvents();
+
+    return {
+        props: {
+            events: events,
+        },
+        revalidate: 60,
+    };
 }
 
 export default AllEventsPage;
