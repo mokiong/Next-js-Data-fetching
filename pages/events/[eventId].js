85681c290fbccc98531ab5fb455bcf7ceb2b1b3c
Moki <mokiong1427@gmail.com>
2021-10-23 11:27:01 +0800

    Create api util

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -8,33 +8,33 @@ import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 
 function EventDetailPage() {
-  const router = useRouter();
+    const router = useRouter();
 
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
 
-  if (!event) {
+    if (!event) {
+        return (
+            <ErrorAlert>
+                <p>No event found!</p>
+            </ErrorAlert>
+        );
+    }
+
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+        <Fragment>
+            <EventSummary title={event.title} />
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     );
-  }
-
-  return (
-    <Fragment>
-      <EventSummary title={event.title} />
-      <EventLogistics
-        date={event.date}
-        address={event.location}
-        image={event.image}
-        imageAlt={event.title}
-      />
-      <EventContent>
-        <p>{event.description}</p>
-      </EventContent>
-    </Fragment>
-  );
 }
 
 export default EventDetailPage;
